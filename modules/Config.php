8a5b869f411a99026fc0a0452acb11ff835645d4
huchao <hu_chao@139.com>
2016-07-22 11:17:49 +0800

    add config

---
+++ modules/Config.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * simple config
+ */
+namespace cmhc\amber\modules;
+
+class Config
+{
+
+	private static $config;
+
+	/**
+	 * get config
+	 * @param  string $key
+	 * @return mixed
+	 */
+	public static function get($key)
+	{
+		if( isset(self::$config[$key]) ){
+			return self::$config[$key];
+		}else{
+			return false;
+		}
+	}
+
+
+	/**
+	 * add config
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	public static function set($key, $value)
+	{
+		if( !isset(self::$config[$key]) ){
+			self::$config[$key] = $value;
+		}else{
+			throw new \Exception("config $key is exists", 1);
+		}
+	}
+
+}
+?>
